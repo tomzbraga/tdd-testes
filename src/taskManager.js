66d7014eeb1e952beb195cb0a90cdb097668db13
Lucas Tomaz <tomzbraga@gmail.com>
2026-04-15 21:24:20 -0300

    green: implementa addTask

--- src/taskManager.js
+++ src/taskManager.js
@@ -35,3 +35,18 @@ export function createTask(title) {
     completed: false,
   };
 }
+
+// ------------------------------------------------------------
+// Adição com validação
+// ------------------------------------------------------------
+
+export function addTask(tasks, title) {
+  if (!validateTitle(title)) {
+    throw new Error(
+      'Título inválido: deve ser uma string com pelo menos 3 caracteres.'
+    );
+  }
+
+  const newTask = createTask(title);
+  return [...tasks, newTask];
+}
